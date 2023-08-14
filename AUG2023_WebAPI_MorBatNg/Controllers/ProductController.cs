e750ecfdb75d4f1e06c3e46717fb1768c18af803
elangrg <45620769+elangrg@users.noreply.github.com>
2023-08-14 11:10:02 +0530

    Add project files.

---
+++ AUG2023_WebAPI_MorBatNg/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using AUG2023_WebAPI_MorBatNg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AUG2023_WebAPI_MorBatNg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        Models.KNDProductDbContext _db;
+        public ProductController(Models.KNDProductDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _db.Products.ToList();
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product GetProductByID(int id)
+        {
+            return _db.Products.Find(id) ;
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void AddNewProduct(Models.Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();  
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void UpdateProduct(int id, Models.Product product)
+        {
+            _db.Products.Update(product);
+            _db.SaveChanges();
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void RemoveProductByID(int id)
+        {
+            _db.Products.Remove(_db.Products.Find(id ));
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ AUG2023_WebAPI_MorBatNg/Models/KNDProductDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AUG2023_WebAPI_MorBatNg.Models
+{
+    public partial class KNDProductDbContext : DbContext
+    {
+        public KNDProductDbContext()
+        {
+        }
+
+        public KNDProductDbContext(DbContextOptions<KNDProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=KNDProductDb;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
